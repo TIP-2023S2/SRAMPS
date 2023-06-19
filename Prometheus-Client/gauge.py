cd008ca1f65a1af3b76017449dcb908b7f743f69
Raju Dahal <dahalraju55@gmail.com>
2023-06-20 00:08:31 +1000

    gauge testing with custom labels

--- Prometheus-Client/gauge.py
+++ Prometheus-Client/gauge.py
@@ -4,12 +4,45 @@ This code snippet demonstrates how to create and update a Gauge metric using the
 from prometheus_client import start_http_server, Gauge
 import time
 
-start_http_server(8000)
+# initialize start_time variable to record function starting time
+start_time = time.time()
 
-# Create a Gauge metric
-gauge_metric = Gauge('my_gauge_metric', 'Description of my gauge metric')
 
-# Update the gauge metric value periodically
-while True:
-    gauge_metric.set(40)  # Update the value
-    time.sleep(1)  # Sleep for 1 second
+def test_gauge(value):
+    """
+    Start an HTTP server on port 8000 and set a gauge value of 100 to be sent on the server
+    Args:
+         gauge_metric: name of the gauge metric being created
+    Returns:
+       Returns a success message after successfully running for 100 seconds
+    Raises:
+          Server Creation Error with related error message
+    """
+    try:
+        # Create a Gauge metric
+        gauge_metric = Gauge(
+            'my_gauge_metric',
+            'Test gauge metric',
+            ['label_name1', 'label_name2']
+        )
+
+        # Start an HTTP server on port 8001
+        start_http_server(8001)
+
+        # Update the gauge metric value
+        while True:
+            # gauge_metric.set(value)  # Update the value
+            gauge_metric.labels(label_name1='value1', label_name2='value2').set(40)
+            time.sleep(1)  # Sleep for 1 second
+
+            # exit the loop after 100 seconds
+            # if time.time() - start_time >= 100:
+            #     break
+
+    except Exception as e:
+        print(str(e))
+
+    return "Successfully created prometheus server on port 8000"
+
+
+print(test_gauge(1))
